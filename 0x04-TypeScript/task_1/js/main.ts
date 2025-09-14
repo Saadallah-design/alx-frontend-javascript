ef5c7de78753b561de174e064ac85239e002d07f
Saadallah-design <saadalla.publishing@gmail.com>
2025-09-14 17:31:17 +0700

    3rd trial to add print function and teacher and directors interface copied from other repo.

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,13 +1,13 @@
 
 interface Teacher {
-    readonly firstName : string, // using readonly before the property name 
+    readonly firstName: string, // using readonly before the property name 
                                  // prevents the property from being assigned 
                                  // a new value after the object is first initialized
-    fullTimeEmployee : boolean,
-    readonly lastName : string,
-    yearsOfExperience? : number,
-    location : string,
-    contract : any,
+    fullTimeEmployee: boolean,
+    readonly lastName: string,
+    yearsOfExperience?: number,
+    location: string,
+    contract: any,
 
     // creating an index-key to allow addition of any other attribute
     // flexible index signature below
@@ -18,7 +18,7 @@ interface Teacher {
 // creating Directors interface
 // extending on the teachers interface
 interface Directors extends Teacher {   
-    numberOfReports : number;
+    numberOfReports: number;
 }
 
 // Example usage of the Directors interface
